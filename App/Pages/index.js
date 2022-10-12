dd422b953aaefdddc285a8cf25f500c9ff8d6745
Naufal Mahrus Sidik <falnaufalsidik@gmail.com>
2022-10-12 10:56:40 +0700

    import new pages

--- App/Pages/index.js
+++ App/Pages/index.js
@@ -1,5 +1,8 @@
 import HomeScreen from './HomeScreen';
 import SplashScreen from './SplashScreen';
 import LoginScreen from './LoginScreen';
+import EhacScreen from './EhacScreen';
+import ImunizationScreen from './ImunizationScreen';
+import CovidTextScreen from './CovidTextScreen';
 
-export {HomeScreen, SplashScreen, LoginScreen};
+export {HomeScreen, SplashScreen, LoginScreen, EhacScreen, ImunizationScreen, CovidTextScreen};
