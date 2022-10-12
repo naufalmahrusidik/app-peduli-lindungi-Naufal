e78f51377a53d44387c87e3369e4d4d727525780
Naufal Mahrus Sidik <falnaufalsidik@gmail.com>
2022-10-12 10:27:46 +0700

    add gambar

--- App/assets/index.js
+++ App/assets/index.js
@@ -1,8 +1,22 @@
-import IconSplash from './img/logo-with-text.png';
+import IconSplash from './img/logo-with-text1.png';
 import Logo_Kpcpen from './img/logo_kpcpen.png';
 import Logo_Kominfo from './img/logo_kominfo.png';
 import Logo_Kemenkes from './img/logo_kemenkes.png';
 import Logo_Bumn from './img/logo_bumn.png';
+import Logo_Top from './img/logo-top.png';
+import Icon_Hand from './img/ic_hand_qr.jpg';
+import Icon_Vaccine from './img/ic_vaccine_and_immuniazation.jpg';
+import Icon_CovidTestResult from './img/ic_covid_test_result.jpg';
+import Icon_Ehac from './img/ic_ehac.jpg';
+import Icon_Travel from './img/ic_travel_regulation.jpg';
+import Icon_Telemedicine from './img/ic_telemedicine.jpg';
+import Icon_Healthcare from './img/ic_healthcare_facility.jpg';
+import Icon_StatisticCovid from './img/ic_statistic_covid.jpg';
+import Icon_FindHospital from './img/ic_find_hospital.jpg';
+import Icon_User from './img/user.jpeg';
+import Lonceng from './img/lonceng.jpeg';
+import Panah from './img/panah.jpeg';
+import QrCode from './img/qr_code.png';
 
 export {
     IconSplash, 
@@ -10,4 +24,18 @@ export {
     Logo_Kominfo, 
     Logo_Kemenkes, 
     Logo_Bumn, 
+    Logo_Top,
+    Icon_Hand,
+    Icon_Vaccine,
+    Icon_CovidTestResult,
+    Icon_Ehac,
+    Icon_Travel,
+    Icon_Telemedicine,
+    Icon_Healthcare,
+    Icon_StatisticCovid,
+    Icon_FindHospital,
+    Icon_User,
+    Lonceng,
+    Panah,
+    QrCode,
 };
