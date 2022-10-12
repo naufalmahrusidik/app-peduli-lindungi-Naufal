5d296035bb672d4c1241b36a26d38c787d4d429f
Naufal Mahrus Sidik <falnaufalsidik@gmail.com>
2022-10-12 08:57:26 +0700

    add import gambar

---
+++ App/assets/index.js
@@ -0,0 +1,13 @@
+import IconSplash from './img/logo-with-text.png';
+import Logo_Kpcpen from './img/logo_kpcpen.png';
+import Logo_Kominfo from './img/logo_kominfo.png';
+import Logo_Kemenkes from './img/logo_kemenkes.png';
+import Logo_Bumn from './img/logo_bumn.png';
+
+export {
+    IconSplash, 
+    Logo_Kpcpen, 
+    Logo_Kominfo, 
+    Logo_Kemenkes, 
+    Logo_Bumn, 
+};
